70c1c42b5971176eab2d37c18edd8ca8e645d046
Nikolas Bodenmüller <nikobo2003@gmail.com>
2025-06-12 11:43:06 +0200

    feat: change printed output and tab size

--- src/main/java/net/nikomitk/pqc/benchmark/Benchmark.kt
+++ src/main/java/net/nikomitk/pqc/benchmark/Benchmark.kt
@@ -8,54 +8,55 @@ import kotlin.random.Random
 import kotlin.time.Duration
 import kotlin.time.measureTime
 
-val results = ArrayList< SignatureUtil.Result>()
+val results = ArrayList<SignatureUtil.Result>()
 
 fun main() {
-    // Register Bouncy Castle providers
-    Security.addProvider(BouncyCastleProvider())
-    Security.addProvider(BouncyCastlePQCProvider())
+  // Register Bouncy Castle providers
+  Security.addProvider(BouncyCastleProvider())
+  Security.addProvider(BouncyCastlePQCProvider())
 
-    // Number of test repetitions
-    val repetitions = 50
+  // Number of test repetitions
+  val repetitions = 50
 
-    // Security level of the signature
-    val securityLevel = SignatureUtil.SecurityLevel.entries
+  // Security level of the signature
+  val securityLevel = SignatureUtil.SecurityLevel.entries
 
-    // Signature schemes to benchmark
-    val algorithms = SignatureUtil.Algorithm.entries
+  // Signature schemes to benchmark
+  val algorithms = SignatureUtil.Algorithm.entries
 
-    println("Starting benchmark with $repetitions repetitions and security level $securityLevel. \nAlgorithms: $algorithms")
+  println("Starting benchmark with $repetitions repetitions and security level $securityLevel. \nAlgorithms: $algorithms")
 
-    // generate random byte arrays for testing
-    val strings = arrayListOf<ByteArray>()
-    repeat(repetitions, {
-        strings.add(Random.nextBytes(10))
-    })
-    for (level in securityLevel) {
-        doBenchmark(level, strings, algorithms)
-    }
+  // generate random byte arrays for testing
+  val strings = arrayListOf<ByteArray>()
+  repeat(repetitions, {
+    strings.add(Random.nextBytes(10))
+  })
+
+  for (level in securityLevel) {
+    doBenchmark(level, strings, algorithms)
+  }
 }
 
 fun doBenchmark(
-    securityLevel: SignatureUtil.SecurityLevel = SignatureUtil.SecurityLevel.LOW,
-    strings: ArrayList<ByteArray>,
-    algorithms: List<SignatureUtil.Algorithm>
+  securityLevel: SignatureUtil.SecurityLevel = SignatureUtil.SecurityLevel.LOW,
+  strings: ArrayList<ByteArray>,
+  algorithms: List<SignatureUtil.Algorithm>
 ) {
 
-    for (algorithm in algorithms) {
-        val signature = SignatureUtil.generateSignature(algorithm, securityLevel)
-        val time = benchAlgorithm(signature, strings)
-        println("$algorithm-${securityLevel.value} total time: $time")
-        results.add(SignatureUtil.Result(algorithm.value, securityLevel, time))
-    }
+  for (algorithm in algorithms) {
+    val signature = SignatureUtil.generateSignature(algorithm, securityLevel)
+    val time = benchAlgorithm(signature, strings)
+    println("$algorithm, ${securityLevel.value}-bits, total time: $time")
+    results.add(SignatureUtil.Result(algorithm.value, securityLevel, time))
+  }
 }
 
 fun benchAlgorithm(signature: Signature, strings: ArrayList<ByteArray>): Duration {
-    var totalTime = Duration.ZERO
-    for (s in strings) {
+  var totalTime = Duration.ZERO
+  for (s in strings) {
 
-        signature.update(s, 0, s.size)
-        totalTime += measureTime { signature.sign() }
-    }
-    return totalTime
+    signature.update(s, 0, s.size)
+    totalTime += measureTime { signature.sign() }
+  }
+  return totalTime
 }
--- src/main/java/net/nikomitk/pqc/benchmark/SignatureUtil.kt
+++ src/main/java/net/nikomitk/pqc/benchmark/SignatureUtil.kt
@@ -11,87 +11,87 @@ import kotlin.time.Duration
 
 object SignatureUtil {
 
-    enum class Algorithm(val value: String) {
-        ECDSA("ECDSA"),
-        RSA("RSA"),
-        DILITHIUM("DILITHIUM"),
-        SPHINCSPlusFast("SPHINCSPlus-f"),
-        SPHINCSPlusSimple("SPHINCSPlus-s");
+  enum class Algorithm(val value: String) {
+    ECDSA("ECDSA"),
+    RSA("RSA"),
+    DILITHIUM("DILITHIUM"),
+    SPHINCSPlusFast("SPHINCSPlus-f"),
+    SPHINCSPlusSimple("SPHINCSPlus-s");
+  }
+
+  enum class SecurityLevel(val value: Int) {
+    LOW(128),
+    MEDIUM(192),
+    HIGH(256),
+  }
+
+  data class Result(val algorithm: String, val securityLevel: SecurityLevel, val time: Duration)
+
+  fun generateSignature(algorithm: Algorithm, securityLevel: SecurityLevel = SecurityLevel.LOW): Signature =
+    when (algorithm) {
+      Algorithm.ECDSA -> createECDSASignature(securityLevel)
+      Algorithm.RSA -> createRSASignature(securityLevel)
+      Algorithm.DILITHIUM -> createDilithiumSignature(securityLevel)
+      Algorithm.SPHINCSPlusFast -> createSPHINCSPlusFastSignature(securityLevel)
+      Algorithm.SPHINCSPlusSimple -> createSPHINCSPlusSimpleSignature(securityLevel)
     }
 
-    enum class SecurityLevel(val value: Int) {
-        LOW(128),
-        MEDIUM(192),
-        HIGH(256),
+  fun createECDSASignature(securityLevel: SecurityLevel = SecurityLevel.LOW): Signature {
+    val paramSpec = when (securityLevel) {
+      SecurityLevel.LOW -> ECNamedCurveGenParameterSpec("P-256")
+      SecurityLevel.MEDIUM -> ECNamedCurveGenParameterSpec("P-384")
+      SecurityLevel.HIGH -> ECNamedCurveGenParameterSpec("P-521")
     }
+    val ecdsa = createGenericSignature("ECDSA", paramSpec)
+    return ecdsa
+  }
 
-    data class Result(val algorithm: String, val securityLevel: SecurityLevel, val time: Duration)
-
-    fun generateSignature(algorithm: Algorithm, securityLevel: SecurityLevel = SecurityLevel.LOW): Signature =
-        when (algorithm) {
-            Algorithm.ECDSA -> createECDSASignature(securityLevel)
-            Algorithm.RSA -> createRSASignature(securityLevel)
-            Algorithm.DILITHIUM -> createDilithiumSignature(securityLevel)
-            Algorithm.SPHINCSPlusFast -> createSPHINCSPlusFastSignature(securityLevel)
-            Algorithm.SPHINCSPlusSimple -> createSPHINCSPlusSimpleSignature(securityLevel)
-        }
-
-    fun createECDSASignature(securityLevel: SecurityLevel = SecurityLevel.LOW): Signature {
-        val paramSpec = when (securityLevel) {
-            SecurityLevel.LOW -> ECNamedCurveGenParameterSpec("P-256")
-            SecurityLevel.MEDIUM -> ECNamedCurveGenParameterSpec("P-384")
-            SecurityLevel.HIGH -> ECNamedCurveGenParameterSpec("P-521")
-        }
-        val ecdsa = createGenericSignature("ECDSA", paramSpec)
-        return ecdsa
+  fun createRSASignature(securityLevel: SecurityLevel = SecurityLevel.LOW): Signature {
+    val paramSpec = when (securityLevel) {
+      SecurityLevel.LOW -> RSAKeyGenParameterSpec(3072, RSAKeyGenParameterSpec.F4)
+      SecurityLevel.MEDIUM -> RSAKeyGenParameterSpec(7680, RSAKeyGenParameterSpec.F4)
+      SecurityLevel.HIGH -> RSAKeyGenParameterSpec(15360, RSAKeyGenParameterSpec.F4)
     }
+    val rsa = createGenericSignature("RSA", paramSpec)
+    return rsa
+  }
 
-    fun createRSASignature(securityLevel: SecurityLevel = SecurityLevel.LOW): Signature {
-        val paramSpec = when (securityLevel) {
-            SecurityLevel.LOW -> RSAKeyGenParameterSpec(3072, RSAKeyGenParameterSpec.F4)
-            SecurityLevel.MEDIUM -> RSAKeyGenParameterSpec(7680, RSAKeyGenParameterSpec.F4)
-            SecurityLevel.HIGH -> RSAKeyGenParameterSpec(15360, RSAKeyGenParameterSpec.F4)
-        }
-        val rsa = createGenericSignature("RSA", paramSpec)
-        return rsa
+  fun createDilithiumSignature(securityLevel: SecurityLevel = SecurityLevel.LOW): Signature {
+    val paramSpec = when (securityLevel) {
+      SecurityLevel.LOW -> DilithiumParameterSpec.dilithium2
+      SecurityLevel.MEDIUM -> DilithiumParameterSpec.dilithium3
+      SecurityLevel.HIGH -> DilithiumParameterSpec.dilithium5
     }
+    val dilithium = createGenericSignature("DILITHIUM", paramSpec)
+    return dilithium
+  }
 
-    fun createDilithiumSignature(securityLevel: SecurityLevel = SecurityLevel.LOW): Signature {
-        val paramSpec = when (securityLevel) {
-            SecurityLevel.LOW -> DilithiumParameterSpec.dilithium2
-            SecurityLevel.MEDIUM -> DilithiumParameterSpec.dilithium3
-            SecurityLevel.HIGH -> DilithiumParameterSpec.dilithium5
-        }
-        val dilithium = createGenericSignature("DILITHIUM", paramSpec)
-        return dilithium
+  fun createSPHINCSPlusFastSignature(securityLevel: SecurityLevel = SecurityLevel.LOW): Signature {
+    val paramSpec = when (securityLevel) {
+      SecurityLevel.LOW -> SPHINCSPlusParameterSpec.sha2_128f
+      SecurityLevel.MEDIUM -> SPHINCSPlusParameterSpec.sha2_192f
+      SecurityLevel.HIGH -> SPHINCSPlusParameterSpec.sha2_256f
     }
+    val sphincsPlus = createGenericSignature("SPHINCSPlus", paramSpec)
+    return sphincsPlus
+  }
 
-    fun createSPHINCSPlusFastSignature(securityLevel: SecurityLevel = SecurityLevel.LOW): Signature {
-        val paramSpec = when (securityLevel) {
-            SecurityLevel.LOW -> SPHINCSPlusParameterSpec.sha2_128f
-            SecurityLevel.MEDIUM -> SPHINCSPlusParameterSpec.sha2_192f
-            SecurityLevel.HIGH -> SPHINCSPlusParameterSpec.sha2_256f
-        }
-        val sphincsPlus = createGenericSignature("SPHINCSPlus", paramSpec)
-        return sphincsPlus
+  fun createSPHINCSPlusSimpleSignature(securityLevel: SecurityLevel = SecurityLevel.LOW): Signature {
+    val paramSpec = when (securityLevel) {
+      SecurityLevel.LOW -> SPHINCSPlusParameterSpec.sha2_128s
+      SecurityLevel.MEDIUM -> SPHINCSPlusParameterSpec.sha2_192s
+      SecurityLevel.HIGH -> SPHINCSPlusParameterSpec.sha2_256s
     }
+    val sphincsPlus = createGenericSignature("SPHINCSPlus", paramSpec)
+    return sphincsPlus
+  }
 
-    fun createSPHINCSPlusSimpleSignature(securityLevel: SecurityLevel = SecurityLevel.LOW): Signature {
-        val paramSpec = when (securityLevel) {
-            SecurityLevel.LOW -> SPHINCSPlusParameterSpec.sha2_128s
-            SecurityLevel.MEDIUM -> SPHINCSPlusParameterSpec.sha2_192s
-            SecurityLevel.HIGH -> SPHINCSPlusParameterSpec.sha2_256s
-        }
-        val sphincsPlus = createGenericSignature("SPHINCSPlus", paramSpec)
-        return sphincsPlus
-    }
-
-    private fun createGenericSignature(algorithm: String, paramSpecs: AlgorithmParameterSpec): Signature {
-        val keyPairGenerator = KeyPairGenerator.getInstance(algorithm, "BC")
-        keyPairGenerator.initialize(paramSpecs)
-        val keyPair = keyPairGenerator.generateKeyPair()
-        val signature = Signature.getInstance(algorithm)
-        signature.initSign(keyPair.private)
-        return signature
-    }
+  private fun createGenericSignature(algorithm: String, paramSpecs: AlgorithmParameterSpec): Signature {
+    val keyPairGenerator = KeyPairGenerator.getInstance(algorithm, "BC")
+    keyPairGenerator.initialize(paramSpecs)
+    val keyPair = keyPairGenerator.generateKeyPair()
+    val signature = Signature.getInstance(algorithm)
+    signature.initSign(keyPair.private)
+    return signature
+  }
 }
